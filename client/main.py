42ce61a81f6751fdbde04f6712647166e316b610
Highpurity <development@highpurity.cc>
2023-11-27 11:06:10 -0500

    experimenting with box layout

--- client/main.py
+++ client/main.py
@@ -1,12 +1,31 @@
 from kivy.app import App
+from kivy.uix.button import Button
+from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.widget import Widget
 from kivy.uix.label import Label
 
 
-class yourapp(App):
+class YourApp(App):
     def build(self):
-        root_widget = Label(text="Hello world!")
+        layout = BoxLayout(orientation='vertical')
+        b1 = Button(text='button 1')
+        b2 = Button(text='button 2')
+
+        layout.add_widget(b1)
+        layout.add_widget(b2)
+
+        return layout
+
+
+YourApp().run()
+
+
+"""
+        root_widget = Label()
+        root_widget.text = '[color=#ff0000]Hello[/color] [color=#00ff00]world![/color]'
+        root_widget.font_size = 100
+        root_widget.italic = True
+        root_widget.markup = True
+
         return root_widget
-
-
-yourapp().run()
+"""
