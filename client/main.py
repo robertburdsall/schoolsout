25d58c050cf5e68f7fe796d0f6c44e2c111d50fc
Highpurity <development@highpurity.cc>
2023-11-29 10:38:21 -0500

    experimenting with box layout

--- client/main.py
+++ client/main.py
@@ -4,18 +4,24 @@ from kivy.uix.button import Button
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.widget import Widget
 from kivy.uix.label import Label
-
+import matplotlib as plt
+import numpy as np
 
 class YourApp(App):
     def build(self):
         layout = BoxLayout(orientation='vertical')
         title = Label()
         title.text = "Wait time: x"
-        title.pos()
+        x = np.linspace(0, 2 * np.pi, 200)
+        y = np.sin(x)
 
-        graph = Rectangle()
+        fig, ax = plt.subplots()
+        ax.plot(x, y)
+
+        graph = plt.show()
 
         layout.add_widget(title)
+        layout.add_widget(graph)
 
         return layout
 
